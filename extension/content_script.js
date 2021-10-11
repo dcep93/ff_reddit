33205a9c3bc149e9287c35f0c4031fb95e529dbc
dcep93 <dan@quizlet.com>
2021-10-11 13:56:11 -0700

    x

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,5 +1,3 @@
-const version = "2021";
-
 console.log("content_script", location.href);
 const start = new Date().getTime();
 
@@ -15,69 +13,21 @@ function main() {
   f().then(() => setTimeout(main, 100));
 }
 
-const data = { posts: {}, players: {} };
+var data;
 
-function init() {
-  chrome.storage.sync.get(["version"], (result) => {
-    if (result.version !== version) {
-      console.log("clearing");
-      chrome.storage.sync.clear(() =>
-        chrome.storage.sync.set({ version }, () => {
-          run();
-        })
-      );
-    } else {
-      run();
-    }
+function run() {
+  chrome.storage.local.get(["data"], (result) => {
+    data = result.data
+      ? result.data
+      : { posts: {}, players: {}, fetched: { timestamp: -1 } };
+    loadPlayers().then(main);
   });
 }
 
-function run() {
-  loadPlayers().then(main);
-}
-
-function clean(str) {
-  return str
-    .toLowerCase()
-    .replaceAll(/\./g, "")
-    .replaceAll(/ jr\b/g, "")
-    .replaceAll(/ sr\b/g, "")
-    .replaceAll(/ i+\b/g, "")
-    .replaceAll(/\bcmc\b/g, "christian mccaffrey");
-}
-
-function updateHidden(e, key) {
-  if (!location.href.includes("/comments/"))
-    e.style.display = data.posts[key].hidden ? "none" : "";
-}
-
-function updatePlayers(playersDiv, key, redditId) {
-  return Promise.resolve(data.posts[key].players)
-    .then(Object.keys)
-    .then((players) =>
-      players.map((p) => {
-        const playerId = `p_${p}`;
-        const d = document.createElement("span");
-        d.style.paddingRight = "10px";
-        d.innerText = `${data.players[p].n}/${data.players[p].o.toFixed(2)}`;
-        chrome.storage.sync.get([playerId], (result) => {
-          d.title = Object.keys(result[playerId]);
-        });
-        d.onclick = () => {
-          delete data.posts[key].players[p];
-          updatePlayers(playersDiv, key, redditId);
-          chrome.storage.sync.get([playerId], (result) => {
-            const redditIds = result[playerId];
-            delete redditIds[redditId];
-            console.log(`removing ${playerId} from ${redditId}`);
-            chrome.storage.sync.set({ [playerId]: redditIds });
-            chrome.storage.sync.set({ [key]: data.posts[key] });
-          });
-        };
-        return d;
-      })
-    )
-    .then((playerDivs) => playersDiv.replaceChildren(...playerDivs));
+function saveData(passThrough) {
+  return new Promise((resolve, reject) =>
+    chrome.storage.local.set({ data }, () => resolve(passThrough))
+  );
 }
 
 function transform() {
@@ -107,16 +57,17 @@ function transform() {
               })
               .map((e) => transformPost(e, table))
           )
+          .then((promises) => Promise.all(promises))
       )
     )
     .then((promises) => Promise.all(promises))
+    .then(saveData)
     .then((arrs) => arrs.flatMap((i) => i))
     .then((es) => es.length && log(es));
 }
 
 function transformPost(e, table) {
   const redditId = e.getAttribute("data-fullname");
-  const key = `r_${redditId}`;
 
   const wrapper = document.createElement("div");
   wrapper.style.padding = "3px";
@@ -128,35 +79,33 @@ function transformPost(e, table) {
   const controls = document.createElement("span");
   controls.innerText = `${
     e.getElementsByClassName("comments")[0].innerText
-  } - ${key}`;
+  } - ${redditId}`;
   controls.onclick = () => {
-    data.posts[key].hidden = !data.posts[key].hidden;
-    updateHidden(e, key);
-    console.log(`toggling ${key} ${data.posts[key].hidden}`);
-    chrome.storage.sync.set({ [key]: data.posts[key] });
+    console.log(`toggling ${redditId} ${data.posts[redditId].hidden}`);
+    data.posts[redditId].hidden = !data.posts[redditId].hidden;
+    updateHidden(e, redditId);
+    saveData();
   };
   wrapper.appendChild(controls);
 
   const timestamp = e.getElementsByTagName("time")[0].title;
 
   const postTitle = e.querySelector("a.title").innerText;
-  boxdiv = getBoxDiv(key, playersDiv, redditId, postTitle, timestamp);
+  boxdiv = getBoxDiv(playersDiv, redditId, postTitle, timestamp);
   e.appendChild(boxdiv);
 
-  chrome.storage.sync.get([key], (result) => {
-    if (result[key] === undefined) {
-      read(postTitle, redditId, key, playersDiv, e, timestamp);
-    } else {
-      data.posts[key] = result[key];
-      updateHidden(e, key);
-      updatePlayers(playersDiv, key, redditId);
-    }
-  });
+  var p = Promise.resolve();
+  if (data.posts[redditId] === undefined) {
+    p = p.then(() => read(postTitle, redditId, playersDiv, timestamp));
+  } else {
+    updateHidden(e, redditId);
+    p = p.then(() => updatePlayers(playersDiv, redditId));
+  }
   wrapper.appendChild(e);
-  return e;
+  return p.then(() => e);
 }
 
-function getBoxDiv(key, playersDiv, redditId, title, timestamp) {
+function getBoxDiv(playersDiv, redditId, title, timestamp) {
   const boxdiv = document.createElement("div");
   const box = document.createElement("input");
   const boxplayers = document.createElement("span");
@@ -176,24 +125,16 @@ function getBoxDiv(key, playersDiv, redditId, title, timestamp) {
             d.onclick = () => {
               boxplayers.replaceChildren();
               box.value = "";
-              data.posts[key].players[p.id] = new Date().getTime();
-              updatePlayers(playersDiv, key, redditId);
-              const playerId = `p_${p.id}`;
-              chrome.storage.sync.get([playerId], (result) => {
-                const redditIds = result[playerId] || {};
-                redditIds[redditId] = {
+              data.posts[redditId].players[p.id] = new Date().getTime();
+              data.players[p.id] = Object.assign(data.players[p.id] || {}, {
+                [redditId]: {
                   redditId,
                   title,
                   timestamp,
-                };
-                console.log(`saving ${playerId} to ${redditId}`);
-                chrome.storage.sync.set({
-                  [playerId]: redditIds,
-                });
-                chrome.storage.sync.set({
-                  [key]: data.posts[key],
-                });
+                },
               });
+              updatePlayers(playersDiv, redditId);
+              saveData();
             };
             return d;
           })
@@ -202,12 +143,10 @@ function getBoxDiv(key, playersDiv, redditId, title, timestamp) {
   return boxdiv;
 }
 
-function read(postTitle, redditId, key, playersDiv, e, timestamp) {
+function read(title, redditId, playersDiv, timestamp) {
   return Promise.resolve(data.players)
     .then(Object.values)
-    .then((players) =>
-      players.filter((p) => clean(postTitle).includes(clean(p.n)))
-    )
+    .then((players) => players.filter((p) => clean(title).includes(clean(p.n))))
     .then((players) => {
       players.length &&
         console.log(`reading ${players.map((p) => p.n)} to ${redditId}`);
@@ -215,91 +154,66 @@ function read(postTitle, redditId, key, playersDiv, e, timestamp) {
     })
     .then((players) => players.map((p) => [p.id, new Date().getTime()]))
     .then(Object.fromEntries)
-    .then((players) =>
-      chrome.storage.sync.set({ [key]: { players } }, () => {
-        data.posts[key] = { players };
-        updateHidden(e, key);
-        Promise.resolve(Object.keys(players))
-          .then((p) =>
-            p
-              .map((p) => `p_${p}`)
-              .map((playerId) =>
-                seriallyUpdate(playerId, redditId, postTitle, timestamp)
-              )
+    .then((players) => {
+      data.posts[redditId] = { players };
+      Promise.resolve(players)
+        .then(Object.keys)
+        .then((p) =>
+          p.map(
+            (playerId) =>
+              (data.players[playerId] = Object.assign(
+                data.players[playerId] || {},
+                {
+                  [redditId]: {
+                    redditId,
+                    title,
+                    timestamp,
+                  },
+                }
+              ))
           )
-          .then((ps) => Promise.all(ps))
-          .then(() => updatePlayers(playersDiv, key, redditId));
-      })
-    );
-}
-
-const promises = [Promise.resolve()];
-function seriallyUpdate(playerId, redditId, title, timestamp) {
-  const x = {};
-  function helper(x, resolve) {
-    if (x.p === undefined) return setTimeout(() => helper(x, resolve), 10);
-    promises.splice(0, 1, x.p)[0].then(() =>
-      chrome.storage.sync.get([playerId], (result) => {
-        const redditIds = result[playerId] || {};
-        redditIds[redditId] = {
-          redditId,
-          title,
-          timestamp,
-        };
-        chrome.storage.sync.set(
-          {
-            [playerId]: redditIds,
-          },
-          resolve
-        );
-      })
-    );
-  }
-  x.p = new Promise((resolve, reject) => helper(x, resolve));
-  return x.p;
-}
-
-function loadPlayers() {
-  const timestamp = new Date().getTime();
-  return new Promise((resolve, reject) =>
-    chrome.storage.local.get(["playersW"], (result) => {
-      if (
-        result.playersW &&
-        result.playersW.timestamp > timestamp - 6 * 60 * 60 * 1000
-      ) {
-        resolve(result.playersW.players);
-      } else {
-        console.log("fetching", result.playersW?.timestamp);
-
-        fetch(
-          "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/players?scoringPeriodId=0&view=players_wl",
-          {
-            headers: {
-              "x-fantasy-filter": '{"filterActive":{"value":true}}',
-            },
-          }
         )
-          .then((resp) => resp.json())
-          .then((resp) =>
-            resp.map(({ fullName, id, ownership }) => [
-              id,
-              {
-                n: fullName,
-                id,
-                o: ownership?.percentOwned,
-              },
-            ])
-          )
-          .then(Object.fromEntries)
-          .then((players) => {
-            const playersW = { players, timestamp };
-            chrome.storage.local.set({ playersW });
-            return players;
-          })
-          .then(resolve);
-      }
-    })
-  ).then((players) => (data.players = players));
+        .then((ps) => Promise.all(ps))
+        .then(() => updatePlayers(playersDiv, redditId));
+    });
+}
+
+function clean(str) {
+  return str
+    .toLowerCase()
+    .replaceAll(/\./g, "")
+    .replaceAll(/ jr\b/g, "")
+    .replaceAll(/ sr\b/g, "")
+    .replaceAll(/ i+\b/g, "")
+    .replaceAll(/\bcmc\b/g, "christian mccaffrey");
+}
+
+function updateHidden(e, redditId) {
+  if (!location.href.includes("/comments/"))
+    e.style.display = data.posts[redditId].hidden ? "none" : "";
+}
+
+function updatePlayers(playersDiv, redditId) {
+  return Promise.resolve(data.posts[redditId].players)
+    .then(Object.keys)
+    .then((players) =>
+      players.map((playerId) => {
+        const d = document.createElement("span");
+        d.style.paddingRight = "10px";
+        d.innerText = `${data.players[p].n}/${data.players[p].o.toFixed(2)}`;
+        d.title = Object.keys(data.players[playerId]);
+        d.onclick = () => {
+          console.log(`removing ${playerId} from ${redditId}`);
+          delete data.posts[redditId].players[p];
+          const redditIds = data.players[playerId];
+          delete redditIds[redditId];
+          updatePlayers(playersDiv, redditId);
+          saveData();
+        };
+        return d;
+      })
+    )
+    .then((playerDivs) => playersDiv.replaceChildren(...playerDivs));
 }
 
 function inject() {
@@ -321,23 +235,55 @@ function inject() {
     )
     .then((href) => {
       if (!href) return;
-      const id = href.split("_/id/")[1].split("/")[0];
-      const playerId = `p_${id}`;
-      chrome.storage.sync.get([playerId], (result) => {
-        const innerHTML = Object.values(result[playerId] || {})
-          .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp))
-          .map(
-            ({ redditId, title, timestamp }) =>
-              `<div style="padding: 10px"><div><div>${new Date(
-                timestamp
-              )}</div><a href="https://www.reddit.com/r/fantasyfootball/comments/${
-                redditId.split("_")[1]
-              }">${title}</a></div></div>`
-          )
-          .join("");
-        if (div.innerHTML !== innerHTML) div.innerHTML = innerHTML;
-      });
+      const playerId = href.split("_/id/")[1].split("/")[0];
+      const innerHTML = Object.values(data.players[playerId] || {})
+        .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp))
+        .map(
+          ({ redditId, title, timestamp }) =>
+            `<div style="padding: 10px"><div><div>${new Date(
+              timestamp
+            )}</div><a href="https://www.reddit.com/r/fantasyfootball/comments/${
+              redditId.split("_")[1]
+            }">${title}</a></div></div>`
+        )
+        .join("");
+      if (div.innerHTML !== innerHTML) div.innerHTML = innerHTML;
     });
 }
 
-init();
+function loadPlayers() {
+  const timestamp = new Date().getTime();
+  return new Promise((resolve, reject) => {
+    // 6 hours
+    if (data.fetched.timestamp > timestamp - 6 * 60 * 60 * 1000) {
+      resolve();
+      return;
+    }
+    console.log("fetching", data.fetched.timestamp);
+    fetch(
+      "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/players?scoringPeriodId=0&view=players_wl",
+      {
+        headers: {
+          "x-fantasy-filter": '{"filterActive":{"value":true}}',
+        },
+      }
+    )
+      .then((resp) => resp.json())
+      .then((resp) =>
+        resp.map(({ fullName, id, ownership }) => [
+          id,
+          {
+            n: fullName,
+            id,
+            o: ownership?.percentOwned,
+          },
+        ])
+      )
+      .then(Object.fromEntries)
+      .then((players) => (data.fetched = { players, timestamp }))
+      .then(saveData)
+      .then(resolve);
+  });
+}
+
+chrome.storage.local.clear(run);
